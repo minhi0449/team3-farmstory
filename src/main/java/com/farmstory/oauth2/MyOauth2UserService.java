ba4fad9b15cd3b8164ebea999da49278a33ae55f
minhi0449 <minhi0449@gmail.com>
2024-10-01 14:05:30 +0900

    로그아웃 구현 완료/ 구글 클릭시 액세스 요청 불가

--- src/main/java/com/farmstory/oauth2/MyOauth2UserService.java
+++ src/main/java/com/farmstory/oauth2/MyOauth2UserService.java
@@ -1,5 +1,6 @@
 package com.farmstory.oauth2;
 
+
 import com.farmstory.entity.User;
 import com.farmstory.repository.UserRepository;
 import com.farmstory.security.MyUserDetails;
@@ -14,64 +15,107 @@ import org.springframework.stereotype.Service;
 import java.util.Map;
 import java.util.Optional;
 
-@Log4j2
 @Service
+@Log4j2
 @RequiredArgsConstructor
 public class MyOauth2UserService extends DefaultOAuth2UserService {
 
+
     private final UserRepository userRepository;
 
+
     @Override
-    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        log.info("LoadUser...1" + userRequest);
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) {
+        log.info("loadUser..1 : "+userRequest);
 
         String accessToken = userRequest.getAccessToken().getTokenValue();
-        log.info("LoadUser...2" + accessToken);
+        log.info("loadUser..2 accessToken : "+accessToken);
 
-        String provider = userRequest.getClientRegistration().getRegistrationId();
-        log.info("LoadUser...3" + provider);
+        String provider= userRequest.getClientRegistration().getRegistrationId();
+        log.info("loadUser..3 provider : "+provider);
 
-        OAuth2User oAuth2User = super.loadUser(userRequest);
-        log.info("LoadUser...4" + oAuth2User);
 
-        Map<String, Object> attributes = oAuth2User.getAttributes();
-        log.info("LoadUser...5" + attributes);
+        String uid;
+        String name = "";
+        String email="";
+        String nick;
+        Map<String, Object> attributes;
 
-        // 사용자 확인 및 회원가입 처리
-        String email = (String) attributes.get("email");
-        String uid = email.split("@")[0];
-        String name = attributes.get("given_name").toString();
-        String nick = attributes.get("name").toString();
+        if ("google".equals(provider)) {
+            OAuth2User oAuth2User = super.loadUser(userRequest);
+            log.info("loadUser..4 oAuth2User : "+oAuth2User);
 
-        // User user = userRepository.findById(uid).orElse(null);
-        Optional<User> optUser = userRepository.findById(uid);
 
-        if (optUser.isPresent()) {
-            // 회원 존재하면 시큐리티 인증처리 (로그인)
-            User user = optUser.get();
+            attributes  = oAuth2User.getAttributes();
+            log.info("loadUser..5 attributes : "+attributes);
+
+            // 제공자별 사용자 정보 처리
+
+            email = (String) attributes.get("email");
+            uid = email.split("@")[0];
+            name = (String) attributes.get("given_name");
+            nick = (String) attributes.get("name");
+        } else if ("kakao".equals(provider)) {
+            OAuth2User oAuth2User = super.loadUser(userRequest);
+            log.info("loadUser..4 oAuth2User : "+oAuth2User);
+
+
+            attributes  = oAuth2User.getAttributes();
+            log.info("loadUser..5 attributes : "+attributes);
+            Long id = (Long) attributes.get("id");
+            // 카카오 사용자 정보에서 properties 가져오기
+            Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
+            log.info("loadUser..6 properties : "+properties);
+
+            Map<String, Object> kakao_account= (Map<String, Object>) attributes.get("kakao_account");
+            log.info("loadUser..7 kakao_account : "+kakao_account);
+
+            Map<String, Object> profile = (Map<String, Object>) kakao_account.get("profile");
+            log.info("loadUser..8 profile : "+profile);
+
+            email = (String) kakao_account.get("email");
+            log.info("loadUser..9 email : "+email);
+            uid= email.split("@")[0];
+            name = (String) properties.get("nickname");
+            nick = name+id;
+        } else {
+            throw new OAuth2AuthenticationException("Unsupported provider: " + provider);
+        }
+
+        Optional<User> opt= userRepository.findById(uid);
+
+        if(opt.isPresent()){
+
+            //회원이 존재하면
+            User user = opt.get();
+            log.info("loadUser..7 user : "+user);
 
             return MyUserDetails.builder()
                     .user(user)
                     .accessToken(accessToken)
                     .attributes(attributes)
                     .build();
-        }else {
-            // 회원이 존재하지 않으면 회원가입 처리
-            // 구글로 받은 User 정보는 아래 3가지 밖에 없다
+        }else{
+            //회원이 존재하지 않으면 회원가입처리
+
+
             User user = User.builder()
-                            .uid(uid)
-                            .nick(nick)
-                            .email(email)
-                            .name(name)
-                            .build();
+                    .uid(uid)
+                    .name(name)
+                    .role("USER")
+                    .email(email)
+                    .nick(nick)
+                    .build();
 
             userRepository.save(user);
 
-            return MyUserDetails
-                    .builder()
+            return MyUserDetails.builder()
+                    .user(user)
                     .accessToken(accessToken)
                     .attributes(attributes)
                     .build();
         }
+
+
     }
 }
--- src/main/java/com/farmstory/security/MyUserDetailsService.java
+++ src/main/java/com/farmstory/security/MyUserDetailsService.java
@@ -3,13 +3,14 @@ package com.farmstory.security;
 import com.farmstory.entity.User;
 import com.farmstory.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class MyUserDetailsService implements UserDetailsService {
@@ -21,15 +22,17 @@ public class MyUserDetailsService implements UserDetailsService {
 
         // 사용자가 입력한 아이디로 사용자 조회, 비밀번호에 대한 검증은 이전 컴포넌트인 AuthenticationProvider에서 수행
         Optional<User> optUser = userRepository.findById(uid);
-
+        log.info(optUser);
         if(optUser.isPresent()) {
             // 시큐리티 사용자 인증객체 생성 후 반환
             MyUserDetails myUserDetails = MyUserDetails.builder()
                     .user(optUser.get())
                     .build();
+
+            log.info(myUserDetails);
             return myUserDetails;
         }
-
+        log.info("here...");
         // 사용자가 입력한 아이디가 없을 경우
         return null;
     }
