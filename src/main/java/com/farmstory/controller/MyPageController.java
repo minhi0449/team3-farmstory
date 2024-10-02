b48c7a6b725f670ae33570597fd3527ac2abf277
wlgus5946 <wlgus5946@gmail.com>
2024-10-02 17:20:39 +0900

    myinfo,css

--- src/main/java/com/farmstory/controller/MyPageController.java
+++ src/main/java/com/farmstory/controller/MyPageController.java
@@ -1,10 +1,13 @@
 package com.farmstory.controller;
 
+import com.farmstory.dto.CartResponseDTO;
 import com.farmstory.dto.PageResponseDTO;
 import com.farmstory.dto.order.OrderItemWithOrderWithProductResponseDTO;
 import com.farmstory.security.MyUserDetails;
+import com.farmstory.service.CartService;
 import com.farmstory.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -13,12 +16,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 
+
+@Log4j2
 @Controller
 @RequestMapping("/my-page")
 @RequiredArgsConstructor
 public class MyPageController {
     private final OrderService orderService;
+    private final CartService cartService;
 
     @GetMapping("/order")
     public String order(@PageableDefault(size = 10,page = 0) Pageable pageable, Model model, @AuthenticationPrincipal MyUserDetails userDetails) {
@@ -32,5 +39,17 @@ public class MyPageController {
         model.addAttribute("orderItemPage", orderItemsByUid );
         model.addAttribute("orderItemCount", orderItemCount );
         return "/my-page/order";
+    }
+    @GetMapping("/cart")
+    public String cart(@AuthenticationPrincipal MyUserDetails userDetails, Model model){
+        long count = cartService.count();
+        model.addAttribute("count", count);
+        String uid = String.valueOf(userDetails.getUser().getUid());
+        log.info("uid :" + uid);
+        List<CartResponseDTO> cartdto = cartService.selectAllCartByUid(uid);
+        log.info("cartdto : " + cartdto);
+        model.addAttribute("cart", cartdto);
+
+        return "/my-page/cart";
     }
 }
